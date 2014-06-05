6f2c9fc1971fb4de147df38cdd49d85c14a031a4
Cecilia De Conto <cecilia.de.conto@accenture.com>
2014-06-05 13:44:55 +0200

    First three trigger added!

--- IN/scripts/scripts.js
+++ IN/scripts/scripts.js
@@ -1,16 +1,24 @@
 function includeTweens () {
 	console.log("ci sono");
 	var controller;
+	var scene_1;
+	var scene_bottom;
 	// init controller
 	controller = new ScrollMagic();
 
-	var tween = TweenMax.to("#bottom", 0.5, {autoAlpha:1});
+	var tween_1 = TweenMax.fromTo("#event_1", 1, {left: -1000,}, {left: -122, repeat: 0, ease: Circ.easeInOut});
+	var tween_2 = TweenMax.fromTo("#event_2", 1, {right: -1000,}, {right: -65, repeat: 0, ease: Circ.easeInOut});
+	var tween_bottom = TweenMax.to("#bottom", 0.5, {autoAlpha:1});
 
-	// build scene
-	var scene = new ScrollScene({triggerElement: "#trigger_bottom"})
-					.setTween(tween)
-					.addTo(controller);
+	
+
+	controller.addScene([
+    scene_1 = new ScrollScene({triggerElement: "#trigger_1"}).setTween(tween_1),
+    scene_2 = new ScrollScene({triggerElement: "#trigger_2"}).setTween(tween_2),
+    scene_bottom = new ScrollScene({triggerElement: "#trigger_bottom"}).setTween(tween_bottom)
+]);
 
 	// show indicators (requires debug extension)
-		scene.addIndicators();
+	scene_1.addIndicators();
+	scene_2.addIndicators();
 }
